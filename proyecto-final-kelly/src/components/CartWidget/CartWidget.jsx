7e0481f3a0aef0e924ee493ce93ab995c4f10877
mariano Kelly <marianokellymail@gmail.com>
2022-04-21 23:51:28 -0300

    Componentes I

--- proyecto-final-kelly/src/components/CartWidget/CartWidget.jsx
+++ proyecto-final-kelly/src/components/CartWidget/CartWidget.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './CartWidget.css'
 import { FaShoppingCart } from 'react-icons/fa';
 
 class CartWidget extends React.Component {
   render() {
-    return <a href='#'  id='CartWidget'><FaShoppingCart /> </a>
+    return <a className='carro' href='#'  id='CartWidget'><FaShoppingCart /> </a>
   }
 }
 
